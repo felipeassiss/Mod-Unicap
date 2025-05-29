ff2104df8ecef775e436cf3b879bf8d98510dc4b
Felipe <felipeassisfsantos@gmail.com>
2025-05-29 20:23:18 -0300

    Mod - Unicap Projeto Final de POO.

--- src/main/java/net/goldi/unicapmod/UnicapMod.java
+++ src/main/java/net/goldi/unicapmod/UnicapMod.java
@@ -1,74 +1,84 @@
 package net.goldi.unicapmod;
 
 import com.mojang.logging.LogUtils;
+import net.goldi.unicapmod.block.ModBlocks;
+import net.goldi.unicapmod.datagen.*;
+import net.goldi.unicapmod.item.ModCreativeModeTabs;
 import net.goldi.unicapmod.item.ModItems;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraftforge.api.distmarker.Dist;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.entity.item.ItemEntity; // Import para ItemEntity
+import net.minecraft.world.entity.animal.Chicken; // Import para Chicken
+import net.minecraft.world.item.ItemStack; // Import para ItemStack
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.event.entity.living.LivingDropsEvent; // Import para LivingDropsEvent
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+import java.util.Random; // Import para Random
+import java.util.concurrent.CompletableFuture;
+
 @Mod(UnicapMod.MOD_ID)
 public class UnicapMod {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "unicapmod";
-    // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
+    private final Random random = new Random(); // Instância de Random para calcular a chance
 
-    public UnicapMod(FMLJavaModLoadingContext context)
-    {
+    public UnicapMod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::gatherData);
 
         ModItems.register(modEventBus);
+        ModCreativeModeTabs.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Código de setup comum
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-            event.accept(ModItems.PENA);
-        }
+    public void gatherData(final GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        generator.addProvider(event.includeServer(), new ModBlockTagsProvider(packOutput, lookupProvider, existingFileHelper));
+        generator.addProvider(event.includeServer(), ModLootTableProvider.create(packOutput, lookupProvider));
+        generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput, lookupProvider));
+        generator.addProvider(event.includeServer(), new ModWorldGenProvider(packOutput, lookupProvider));
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-
+    public void onServerStarting(ServerStartingEvent event) {
+        // Código para quando o servidor inicia
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
+    @SubscribeEvent
+    public void onChickenDrops(LivingDropsEvent event) {
+        // Verifica se a entidade que morreu é uma galinha
+        if (event.getEntity() instanceof Chicken) {
+            // Calcula a chance de 5%
+            if (random.nextFloat() < 0.05F) { // 0.05F representa 5%
+                ItemStack magicFeatherDrop = new ItemStack(ModItems.PENA.get(), 1); // Pega 1 Pena
 
+                double x = event.getEntity().getX();
+                double y = event.getEntity().getY();
+                double z = event.getEntity().getZ();
+                event.getDrops().add(new ItemEntity(event.getEntity().level(), x, y, z, magicFeatherDrop));
+            }
         }
     }
 }
